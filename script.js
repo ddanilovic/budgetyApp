ff0166799769fec55a1fa9193e8e9c1970fdfae4
Dusan <danilovicdusan7@gmail.com>
2020-02-24 20:42:52 +0100

    intial commit

--- script.js
+++ script.js
@@ -12,11 +12,45 @@ var budgetController = (function(){
             console.log(add(b));
         }
     }
+    //UI controller
     var UIcontroller = (function(){})();
-    var controller = (function(){})();
 
+    //GLOBAL APP CONTROLLER
+    var controller = (function(budgetCtrl, UICtrl){
+
+        var ctrlAddItem = function(){
+            //1. Get the field input data
+
+            //2. Add the item to the budget controller
+
+            //3. Add the item to the UI
+
+            //4. Calculate the budget
+
+            //5.Display the budget on the UI 
+            console.log('it works')
+        }
+
+        document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    })();
+    document.addEventListener('keypress',function(event){
+
+        if(event.keyCode === 13 || event.which === 13) { //we put || bc of older browsers
+           //1. Get the field input data
+
+            //2. Add the item to the budget controller
+
+            //3. Add the item to the UI
+
+            //4. Calculate the budget
+
+            //5.Display the budget on the UI 
+        }
+
+    });
 })();    //IIFE that will return an object
 
 console.log('09.02.2020');
 console.log('10.02.2020');
 console.log('13.02.2020');
+console.log('24.02.2020');
